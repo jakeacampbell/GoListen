bce56bfde09151a7bde2928465c94c7cd08fded9
jcampbell-mst <97438737+jcampbell-mst@users.noreply.github.com>
2025-04-12 20:48:01 +0100

    Added dependency on beep library

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("GoListen")
+
+	file, err := os.Open("GoListen.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	streamer, format, err := mp3.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	speaker.Play(streamer)
+
+	select {}
 }
